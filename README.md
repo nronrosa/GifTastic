ba75edcc68020dff7ae9551b86b383827c14b7ff
Nitza <rondon.nitza@gmail.com>
2019-03-04 16:29:49 -0500

    Updates

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # GifTastic
+
+### What the project does
+Use the GIPHY API to make a dynamic web page that populates with gifs of your choice. Calls the GIPHY API and use JavaScript and jQuery to change the HTML of your site and allow for users to add their own term to return gifs.
+
+### Why the project is useful
+This project uses CSS, Bootstrap, JavaScript, jQuery, functions, Ajax and GIPHY API.(https://developers.giphy.com/docs/)
+
+### How users can get started with the project
+View this video to see a game in action: (https://youtu.be/BqreERTLjgQ)
+
+### How the site functions
+* There is a theme of preset array of strings
+* Using the array the buttons are created dynamically on the page. 
+* When the user clicks on a button, the page should gets 10 static, non-animated gif images from the GIPHY API and place them on the page.
+* When the user clicks one of the still GIPHY images, the gif should animate. The user clicks the gif again, it should stop playing.
+* Under every gif, display its rating (PG, G, so on).
+* There is a form that takes the value from a user in the input box and adds it into the preset array. Then makes a function call that takes each topic in the array remakes the buttons on the page.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,86 +2,42 @@ $(document).ready(function () {
     // set variables
     var topics = ["Cherry Blossom", "Chrysanthemum", "Dahlia", "Daisy", "Gazania", "Lotus", "Orchid", "Peony", "Roses", "Tulips", "Water Lilies"];
 
-
-    // ******************************
-    // Adding click event listen listener to all buttons
-    // $("button").on("click", function () {
     function displayGifs() {
         $("#gifs").empty();
         var topic = $(this).attr("data-topic");
-        // var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=vf4EK7hbA3pOFo1EFcPqh1cW52hsodu1&q=" + topic + "&limit=10&rating=G&offset=0&rating=G&lang=en";
-        // Performing an AJAX request with the queryURL
-        $.ajax({
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=vf4EK7hbA3pOFo1EFcPqh1cW52hsodu1&q=" + topic + "&limit=10&rating=G&offset=0&rating=G&lang=en"; $.ajax({
             url: queryURL,
             method: "GET"
         })
-            // After data comes back from the request
             .done(function (response) {
-                console.log(queryURL);
-                console.log(response);
-                // storing the data from the AJAX request in the results variable
                 var results = response.data;
-                // debugger;
-                // Looping through each result item
                 for (var i = 0; i < results.length; i++) {
-                    // debugger;
-                    // Creating and storing a div tag
-                    var flowerDiv = $("<div col='col-4'>");
-                    // Creating a paragraph tag with the result item's rating
-                    var p = $("<p>").text("Rating: " + results[i].rating);
-                    // Creating and storing an image tag
+                    var flowerDiv = $("<div>");
                     var flowerImage = $("<img>");
-                    // image = '<div class="col-md-4">' + image + "</div>";
-                    // Setting the src attribute of the image to a property pulled off the result item
+                    var p = $("<p>").text("Rating: " + results[i].rating);
                     flowerImage.attr("src", results[i].images.fixed_height_still.url);
                     flowerImage.attr("data-state-still", results[i].images.fixed_height_still.url);
                     flowerImage.attr("data-state-animate", results[i].images.fixed_height.url);
                     flowerImage.attr("data-state", "still");
                     flowerImage.addClass("flower-img");
-                    // Appending the paragraph and image tag
                     flowerDiv.append(p);
                     flowerDiv.append(flowerImage);
-                    // Prependng to the HTML page in the "#gifs-appear-here" div
                     $("#gifs").prepend(flowerDiv);
-                    // }
-
                 };
                 $(".flower-img").on("click", function () {
-                    debugger;
-                    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
                     var state = $(this).attr("data-state");
                     if (state === "still") {
                         $(this).attr("src", $(this).attr("data-state-animate"));
-                        console.log(this);
                         $(this).attr("data-state", "animate");
                     } else {
                         $(this).attr("src", $(this).attr("data-state-still"));
-                        console.log(this);
                         $(this).attr("data-state", "still");
                     };
                 });
-            
             });
     };
 
-    $(".flower-img").on("click", function () {
-        debugger;
-        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-        var state = $(this).attr("data-state");
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-state-animate"));
-            console.log(this);
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", $(this).attr("data-state-still"));
-            console.log(this);
-            $(this).attr("data-state", "still");
-        };
-    });
-
     function renderButtons() {
-        // debugger;
         $("#created-buttons").empty();
         for (var i = 0; i < topics.length; i++) {
             var button = $("<button>");
@@ -92,31 +48,14 @@ $(document).ready(function () {
         };
     };
 
-     // This function handles events where one button is clicked
-     $("#add-flower").on("click", function(event) {
+    $("#add-flower").on("click", function (event) {
         event.preventDefault();
-        // This line grabs the input from the textbox
         var flower = $("#flower-input").val().trim();
-        // Adding the movie from the textbox to our array
         topics.push(flower);
-        console.log(topics);
-        // Calling renderButtons which handles the processing of our movie array
         renderButtons();
-      });
+    });
 
-
-
-
-
-
-    // Function for displaying the movie info
-    // Using $(document).on instead of $(".movie").on to add event listeners to dynamically generated elements
     $(document).on("click", ".flower-btn", displayGifs);
 
-   
     renderButtons();
-
-
-
-
 });
